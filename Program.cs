0437b989366eb9299e06716d9a32dc1f98998422
Marcella Amorim <devabefarma4@outlook.com>
2024-04-30 08:44:33 -0300

    organizando funcoes crud de operacoes no banco

--- Program.cs
+++ Program.cs
@@ -12,21 +12,69 @@ class Program
     static void Main(string[] args)
     {
         const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=root@mlasa00";
-        const string sqlQuery = "SELECT [Id], [Title] FROM [Category]";
+
+        var category = new Category();
+        category.Id = Guid.NewGuid();
+        category.Title = "AWS Fundamentals part 2";
+        category.Url = "amazon";
+        category.Description = "Aprenda os fundamentos de uma das plataformas mais utilizadas pelas empresas hoje.";
+        category.Summary = "AWS Cloud";
+        category.Order = 8;
+
+        //Category Id (teste): 55a76f63-2260-4565-bb10-bb1db982681e
 
         using (var connection = new SqlConnection(connectionString)){
-            var categories = connection.Query<Category>(sqlQuery);
+            connection.Open();
 
-            Console.WriteLine($"\n\n\n {categories.Count()} categorias de cursos no banco de dados");
+            //RegisterCategory(connection, category);
+            //DeleteCategory(connection, new Guid("55a76f63-2260-4565-bb10-bb1db982681e"));
+            //UpdateCategory(connection, new Guid("55a76f63-2260-4565-bb10-bb1db982681e"), "Minha nova categoria");
+            GetCategories(connection);
 
-            foreach (var category in categories){
-                //WriteJson(category);
-                Console.WriteLine(category.Title);
-            }
+            connection.Close();
         }
     }
+   
+    static void DeleteCategory(SqlConnection connection, Guid id)
+    {
+        const string deleteSql = "DELETE FROM [Category] WHERE [Id] = @Id";
+        var affected = connection.Execute(deleteSql, new{Id = id});
+        Console.WriteLine($"{affected} registros apagados");
+    }
+    static void UpdateCategory(SqlConnection connection, Guid id, string title){
+        var updateSql = "UPDATE [Category] SET [Title]=@title WHERE [Id]=@Id";
 
-    static void WriteJson(object obj){
+        var rows = connection.Execute(updateSql, new{ Id = id, Title = title});
+        Console.WriteLine($"{rows} registros atualizados");
+    }
+    static void RegisterCategory(SqlConnection connection, Category newCategory)
+    {
+        const string insertSql = "INSERT INTO [Category] VALUES(@Id, @Title, @Url, @Summary, @Order, @Description, @Featured)";
+
+        var affected = connection.Execute(insertSql, new
+        {
+            newCategory.Id,
+            newCategory.Title,
+            newCategory.Url,
+            newCategory.Description,
+            newCategory.Summary,
+            newCategory.Order,
+            newCategory.Featured
+        });
+        
+        Console.WriteLine($"{affected.ToString()} linhas inseridas");
+    }
+    static void GetCategories(SqlConnection connection)
+    {
+        const string selectSql = "SELECT TOP 10 [Id], [Title] FROM [Category]";
+
+        var categories = connection.Query<Category>(selectSql);
+        
+        Console.WriteLine($"{categories.Count()} categorias encontradas");
+        foreach(var category in categories) Console.WriteLine(category.Title);
+    }
+    static void WriteJson(object obj)
+    {
         Console.WriteLine(JsonSerializer.Serialize(obj));
     }
 }
